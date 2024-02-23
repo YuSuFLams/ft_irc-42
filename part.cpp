3b9fa3c42c3a121553752be68b4aa41a9ff890b6
Abdeljalil El Hidaoui <abel-hid@e3r7p13.1337.ma>
2024-02-23 19:19:03 +0100

    add part for check join

--- part.cpp
+++ part.cpp
@@ -6,70 +6,79 @@
 /*   By: abel-hid <abel-hid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 21:09:08 by abel-hid          #+#    #+#             */
-/*   Updated: 2024/02/22 22:31:24 by abel-hid         ###   ########.fr       */
+/*   Updated: 2024/02/23 19:17:58 by abel-hid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Server.hpp"
 
-int Server::PartChannel(std::vector<std::string> strs ,std::map<std::string, Channel *> &channels,  int fd, std::string nickname,  Server &server)
+int Server::PartChannel(std::vector<std::string> strs, std::map<std::string, Channel *> &channels, int fd, std::string nickname, Server &server) 
 {
-    std::string channel_str = strs[1];
-    std::string token;
-    std::stringstream ss(channel_str);
-    std::vector<std::string> all_channels;
-    size_t comma_count = 0;
-
-    // Split the string by comma
-    for (size_t i = 0; i < channel_str.length(); ++i) {
-        if (channel_str[i] == ',') {
-            comma_count++;
-        }
-    }
-
-    if (comma_count == 0) 
-    {
-        all_channels.push_back(channel_str);
-    } 
-    else 
-    {
-        size_t start_pos = 0;
-        for (size_t i = 0; i < channel_str.length(); ++i) {
-            if (channel_str[i] == ',') 
-            {
-                token = channel_str.substr(start_pos, i - start_pos);
-                all_channels.push_back(token);
-                start_pos = i + 1;
-            }
-        }
-        token = channel_str.substr(start_pos);
-        all_channels.push_back(token);
-    }
-
-    if (all_channels.size() != comma_count + 1) 
+    if (strs.size() < 2) 
     {
         return -1;
     }
 
-    for (size_t i = 0; i < all_channels.size(); ++i) 
+    std::stringstream ss(strs[1]);
+    
+    std::vector<std::string> all_channels;
+
+    // If the user is trying to leave multiple channels
+    if (strs[1].find(',') != std::string::npos) 
     {
-        if (channels[all_channels[i]]->getUsers().find(nickname) == channels[all_channels[i]]->getUsers().end()) 
+        std::string token;
+        while (std::getline(ss, token, ',')) 
         {
-            std::string msg = ":" + server.get_hostnames() + " 442 " + nickname + " " + all_channels[i] + " :You're not on that channel\r\n";
+            all_channels.push_back(token);
+        }
+    } 
+    else 
+        all_channels.push_back(strs[1]);
+
+    for (std::vector<std::string>::iterator it = all_channels.begin(); it != all_channels.end(); ++it) 
+    {
+        std::string channel_name = *it;
+        if (channel_name[0] != '#' && channel_name[0] != '&') 
+        {
+            std::string msg = ":" + server.get_hostnames() + " " + server.to_string(ERR_BADCHANMASK) + " " + nickname + " " + channel_name + " :Bad channel mask\r\n";
             send(fd, msg.c_str(), msg.length(), 0);
             continue;
         }
-        channels[all_channels[i]]->removeUser(nickname);
-        std::string msg = ":" + nickname + "!" + nickname + "@" + server.get_hostnames() + " PART " + all_channels[i] + "\r\n";
-        if (channels[all_channels[i]]->getUsers().empty()) 
+
+       // Check if the channel exists
+        if (channels.find(channel_name) == channels.end()) 
         {
-            channels.erase(all_channels[i]);
-        }
-        // send to all users in the channel
-        for (std::set<std::string>::iterator it = channels[all_channels[i]]->getUsers().begin(); it != channels[all_channels[i]]->getUsers().end(); ++it) 
-        {
-            int fd = server.get_fd_users(*it);
+            std::string msg = ":" + server.get_hostnames() + " 403 " + nickname + " " + channel_name + " :No such channel\r\n";
             send(fd, msg.c_str(), msg.length(), 0);
+            continue;
+        }
+
+        Channel *channel = channels[channel_name];
+
+        // Check if the user is in the channel before removing them
+        if (channel->getUsers().find(nickname) == channel->getUsers().end()) 
+        {
+            std::string msg = ":" + server.get_hostnames() + " 442 " + nickname + " " + channel_name + " :You're not on that channel\r\n";
+            send(fd, msg.c_str(), msg.length(), 0);
+            continue;
+        }
+        // Remove the user from the channel
+        channel->removeUser(nickname);
+        // channel->print_users();
+        std::string msg = ":" + nickname + "!" + nickname + "@" + server.get_hostnames() + " PART " + channel_name + "\r\n";
+
+        // Send to all users in the channel
+        for (std::set<std::string>::iterator it = channel->getUsers().begin(); it != channel->getUsers().end(); ++it) 
+        {
+            int user_fd = server.get_fd_users(*it);
+            send(user_fd, msg.c_str(), msg.length(), 0);
+        }
+
+        // If the channel is empty after the user leaves, remove it from the map
+        if (channel->getUsers().empty()) 
+        {
+            delete channel;  // Free the memory if the channel is empty
+            channels.erase(channel_name);
         }
     }
     return 0;
