76137b165910d87359b7c738eb050b784eda1e23
YuSuFLams <yousseflamssieh@gmail.com>
2024-03-14 23:56:38 +0000

    ok

--- main.cpp
+++ main.cpp
@@ -6,19 +6,11 @@
 /*   By: ylamsiah <ylamsiah@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 09:39:17 by araiteb           #+#    #+#             */
-/*   Updated: 2024/03/14 21:21:46 by ylamsiah         ###   ########.fr       */
+/*   Updated: 2024/03/14 23:56:33 by ylamsiah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./server/Server.hpp"
-#define RESET   "\033[0m"
-#define RED     "\033[31m"
-#define GREEN   "\033[32m"
-#define YELLOW  "\033[33m"
-#define BLUE    "\033[34m"
-#define MAGENTA "\033[35m"
-#define CYAN    "\033[36m"
-#define WHITE   "\033[37m"
 
 #define Welcome "\033[33;1m\n\
 ░▒▓████████▓▒░▒▓████████▓▒          ░▒▓█▓▒░▒▓███████▓▒░ ░▒▓██████▓▒░  \n\
